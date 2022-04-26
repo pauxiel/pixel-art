8dbe7268107f50b0338089ac3b63667061829a5d
pauxiel <paul.scholes50@yahoo.com>
2022-04-26 13:17:19 +0100

    first commit

---
+++ designs.js
@@ -0,0 +1,37 @@
+// Select color input
+const colorInput = document.getElementById("colorPicker");
+
+// Select size input
+const sizePicker = document.getElementById("sizePicker");
+// pixelCanvas table
+const pixelCanvas = document.getElementById("pixelCanvas");
+
+sizePicker.addEventListener("submit", makeGrid);
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid(e) {
+  if (pixelCanvas.childElementCount > 0) {
+    pixelCanvas.innerHTML = "";
+  }
+
+  e.preventDefault();
+
+  const numberRow = document.getElementById("inputHeight").value;
+  const numberColumn = document.getElementById("inputWidth").value;
+  // Your code goes here!
+  for (let i = 0; i < numberRow; i++) {
+    const tableRow = document.createElement("tr");
+    for (let j = 0; j < numberColumn; j++) {
+      const tableData = document.createElement("td");
+      tableData.style.backgroundColor = colorInput.value;
+      tableRow.appendChild(tableData);
+    }
+    pixelCanvas.appendChild(tableRow);
+  }
+}
+
+pixelCanvas.addEventListener("mousedown", function(e) {
+  if (e.target.tagName !== "TD") return;
+  e.target.style.backgroundColor = colorInput.value;
+});
